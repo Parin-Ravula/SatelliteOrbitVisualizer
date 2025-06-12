c9b1792602c20ab879a40843f98f5fb68088586b
Parin-Ravula <155329364+Parin-Ravula@users.noreply.github.com>
2025-06-12 15:19:00 +0530

    feat: add plotting feature to show satellite (circular) orbit path

--- Satellite_Orbit_Visualizer_V1.py
+++ Satellite_Orbit_Visualizer_V1.py
@@ -7,4 +7,28 @@ altitude = float(input("How high above the Earth will the satellite's orbit? "))
 eccentricity = float(input("What is the eccentricity of the satellite's orbit (between 0 and 1)? "))
 inclination = float(input("what is the inclination of the satellite's orbit (in degrees)? "))
 
+## Hard-coding for testing
+# altitude = 1100
+# eccentricity = 0.02
+# inclination = 13
 
+## Orbit parameters
+earth_radius = 6378
+total_radius = earth_radius + altitude
+theta = np.linspace(0, 2 * np.pi, 10000)
+
+## Parametric equations
+x = total_radius * np.cos(theta)
+y = total_radius * np.sin(theta)
+
+fig, ax = plt.subplots(figsize=(8, 8))
+earth = plt.Circle((0, 0), radius=6371, color='green', alpha=0.5, label='Earth')
+
+# Add to plot
+plt.plot(x, y)
+ax.add_patch(earth)
+plt.gca().set_aspect('equal', adjustable='box')  
+plt.title("Satellite Orbit Trajectory")
+plt.grid(True)
+plt.legend()
+plt.show()
