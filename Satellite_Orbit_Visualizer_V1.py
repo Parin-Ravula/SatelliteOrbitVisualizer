4ba809a3194ba3ffe6c056124fba634852836179
Parin-Ravula <155329364+Parin-Ravula@users.noreply.github.com>
2025-06-12 13:33:22 +0530

    feat: add variables to store user inputs for satellite orbit specs

--- Satellite_Orbit_Visualizer_V1.py
+++ Satellite_Orbit_Visualizer_V1.py
@@ -1,1 +1,10 @@
-print("Hello")
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+## Storing satellite information provided by user
+altitude = float(input("How high above the Earth will the satellite's orbit? "))
+eccentricity = float(input("What is the eccentricity of the satellite's orbit (between 0 and 1)? "))
+inclination = float(input("what is the inclination of the satellite's orbit (in degrees)? "))
+
+
