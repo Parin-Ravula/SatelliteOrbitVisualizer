62957564e9a843635c607c1108fd874279691282
Parin-Ravula <155329364+Parin-Ravula@users.noreply.github.com>
2025-06-14 22:12:51 +0530

    Start of V4 - incorporating satellites orbit animations

--- Satellite_Orbit_Visualizer_V1.py
+++ Satellite_Orbit_Visualizer_V1.py
@@ -126,7 +126,166 @@ import math
 
 
 
-### VERSION 3
+# ### VERSION 3
+# ### ////////////////////////////////////////////////////////////////
+# fig = plt.figure()
+# ax = fig.add_subplot(projection='3d')
+
+# ## Global variables defined
+# earth_radius = 6378
+# theta = np.linspace(0, 2 * np.pi, 10000)
+
+# ## Generate Earth
+# u = np.linspace(0, 2 * np.pi, 100)
+# v = np.linspace(0, np.pi, 100)
+# earth_x = earth_radius * np.outer(np.cos(u), np.sin(v))
+# earth_y = earth_radius * np.outer(np.sin(u), np.sin(v))
+# earth_z = earth_radius * np.outer(np.ones(np.size(u)), np.cos(v))
+# ax.plot_surface(earth_x, earth_y, earth_z, color="skyblue", alpha=0.5)
+
+# ## Apply rotations to orbit's plane
+# def apply_rotation(angle, axis_of_rotation, x, y, z):
+#     rad = np.deg2rad(angle)
+#     ## Assign correct axis of rotation
+#     if axis_of_rotation == "x":
+#         rotation_matrix = np.matrix([
+#             [1, 0, 0], 
+#             [0, np.cos(rad), -1 * np.sin(rad)], 
+#             [0, np.sin(rad), np.cos(rad)]
+#         ])
+#     elif axis_of_rotation == "z":
+#         rotation_matrix = np.matrix([
+#             [np.cos(rad), -1 * np.sin(rad), 0], 
+#             [np.sin(rad), np.cos(rad), 0], 
+#             [0,0,1]
+#         ])
+        
+#     ## Initialize modified points arrays
+#     x_trajectory = []
+#     y_trajectory = []
+#     z_trajectory = []
+    
+#     ## Apply rotation matrix to each point in the orbit to get the transformed (rotated) trajectory
+#     for i in range(0,10000):
+#         orbit_matrix = np.matrix([x[i], y[i], z[i]])
+#         rotated_matrix = np.round(np.dot(orbit_matrix, rotation_matrix), decimals=10)
+#         x_trajectory.append(rotated_matrix[0, 0])
+#         y_trajectory.append(rotated_matrix[0, 1])
+#         z_trajectory.append(rotated_matrix[0, 2])
+    
+#     ## Save rotated points    
+#     trajectory_plots = {
+#         "x_trajectory": x_trajectory,
+#         "y_trajectory": y_trajectory,
+#         "z_trajectory": z_trajectory
+#     }
+    
+#     return trajectory_plots
+
+# def apply_argument_of_periapsis(angle, x, y, z): 
+#     rad = np.deg2rad(angle)  
+     
+#     ## Find axis of rotation
+#     vec1 = [x[1000] - x[250], y[1000] - y[250], z[1000] - z[250]]
+#     vec2 = [x[5000] - x[250], y[5000] - y[250], z[5000] - z[250]]
+    
+#     ## Take cross product to find normal vector, then normalize
+#     normal_vector = np.cross(vec1, vec2)
+#     normal_vector /= np.linalg.norm(normal_vector)
+    
+#     ## Initialize new trajectory
+#     x_trajectory = []
+#     y_trajectory = []
+#     z_trajectory = []
+    
+#     ## Apply rotation matrix to each point in the orbit to get the transformed (rotated) trajectory
+#     for i in range(0,10000):
+#         v = np.array([x[i], y[i], z[i]])
+#         k = normal_vector
+        
+#         ## Rodrigues' rotation formula
+#         rotated = (v * np.cos(rad) +
+#                    np.cross(k, v) * np.sin(rad) +
+#                    k * (np.dot(k, v)) * (1 - np.cos(rad)))
+
+#         x_trajectory.append(rotated[0])
+#         y_trajectory.append(rotated[1])
+#         z_trajectory.append(rotated[2])
+       
+#     ## Save rotated points    
+#     trajectory_plots = {
+#         "x_trajectory": x_trajectory,
+#         "y_trajectory": y_trajectory,
+#         "z_trajectory": z_trajectory
+#     }
+    
+#     return trajectory_plots  
+    
+# ## Graph circular orbit
+# def graph_circular_orbit(satellite_name, altitude, inclination, raan, periapsis):
+#     total_radius = earth_radius + altitude
+    
+#     ## Define orbit parameters    
+#     x = total_radius * np.cos(theta)
+#     y = total_radius * np.sin(theta) 
+#     z = np.zeros(len(x))
+    
+#     ## Apply rotation about x axis (inclination) and z axis (RAAN), then plot   
+#     x_rotation = apply_rotation(inclination, "x", x, y, z) 
+#     z_rotation = apply_rotation(raan, "z", x_rotation["x_trajectory"], x_rotation["y_trajectory"], x_rotation["z_trajectory"])
+#     periapsis_rotation = apply_argument_of_periapsis(periapsis, z_rotation["x_trajectory"], z_rotation["y_trajectory"], z_rotation["z_trajectory"])  
+#     ax.plot(periapsis_rotation["x_trajectory"], periapsis_rotation["y_trajectory"], periapsis_rotation["z_trajectory"], 
+#             zdir='z', linestyle='--', label=f"{satellite_name}'s orbit trajectory")
+ 
+# ## Graph elliptical orbit 
+# def graph_elliptical_orbit(satellite_name, eccentricity, altitude, inclination, raan, periapsis):
+#     ## Define & calculate both semi major axis and semi minor axis
+#     semi_major_axis = earth_radius + altitude
+#     semi_minor_axis = semi_major_axis * math.sqrt(1 - math.pow(eccentricity, 2))
+    
+#     ## Define orbit parameters    
+#     x = (semi_major_axis) * np.cos(theta)
+#     y = (semi_minor_axis) * np.sin(theta)
+#     z = np.zeros(len(x))
+    
+#     ## Find foci points, center ellipse about negative foci (-c, 0)
+#     c = math.sqrt(math.pow((semi_major_axis), 2) - math.pow((semi_minor_axis), 2))
+#     x += c
+    
+#     ## Apply rotation about x axis (inclination) and z axis (RAAN), then plot 
+#     x_rotation = apply_rotation(inclination, "x", x, y, z)
+#     z_rotation = apply_rotation(raan, "z", x_rotation["x_trajectory"], x_rotation["y_trajectory"], x_rotation["z_trajectory"])
+#     periapsis_rotation = apply_argument_of_periapsis(periapsis, z_rotation["x_trajectory"], z_rotation["y_trajectory"], z_rotation["z_trajectory"])     
+#     ax.plot(periapsis_rotation["x_trajectory"], periapsis_rotation["y_trajectory"], periapsis_rotation["z_trajectory"], 
+#             zdir='z', linestyle='--', label=f"{satellite_name}'s orbit trajectory")
+    
+# while(True):
+#     ## Store satellite information provided by user
+#     satellite_name = input("Enter satellite name: ")
+#     if satellite_name == "stop":
+#         break
+#     eccentricity = float(input("Enter satellite orbit's eccentricity (between 0 and 1): "))
+#     altitude = float(input("Enter satellite's altitude/semi-major-axis above the Earth: "))
+#     inclination = float(input("Enter satellite orbit's inclination (in degrees): "))
+#     raan = float(input("Enter the RAAN (rotation of the orbit around the axis of the Earth (in degrees): "))
+#     periapsis = float(input("Enter the argument of periapsis (rotation of the orbit around the axis of the Earth (in degrees): "))
+    
+#     ## Decide orbit's shape
+#     if eccentricity == 0:    
+#         ## Plot circular orbit trajectory
+#         graph_circular_orbit(satellite_name, altitude, inclination, raan, periapsis)
+#     else:
+#        ## Plot elliptical orbit trajectory 
+#        graph_elliptical_orbit(satellite_name, eccentricity, altitude, inclination, raan, periapsis)
+
+# ax.set_aspect('equal')
+# ax.legend()
+# plt.show()
+# ### ////////////////////////////////////////////////////////////////
+
+
+
+### VERSION 4
 ### ////////////////////////////////////////////////////////////////
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
